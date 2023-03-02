4abf12f095ed6e9c34981e28e03163e23377a2c2
Reece Bailey <80913719+Rbailey8857@users.noreply.github.com>
2023-03-01 18:34:19 -0800

    Fonts and Colors
    Charcoal and Light blue.. thank you chatgpt XD

--- my-project/tailwind.config.js
+++ my-project/tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -8,8 +11,18 @@ module.exports = {
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  theme: {colors: {
+    bg: '#ECEFF1',
+    base: '#424242',
+    black: colors.black,
+    'white': '#ffffff',
+    red: colors.red
+  },
+    extend: {
+      fontFamily: {
+        'gaj': ['GAJ', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
