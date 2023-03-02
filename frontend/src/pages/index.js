772c50b717f6cd07a6a6802f4e717041ae475d9d
Rbailey8857 <80913719+Rbailey8857@users.noreply.github.com>
2023-03-02 01:20:45 -0800

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 import Web3Modal from 'web3modal';
 import { useCallback, useEffect, useReducer } from 'react'
 import { ethers } from 'ethers';
